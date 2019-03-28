e8848a961c7ebd89497854ce1c196902e4de78c2
Mika Äärilä <maarila@gmail.com>
2019-03-28 08:48:48 +0200

    Finish part 1a examples

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello World</p>
+      <p>Hello, {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Arto" age={26 + 10}/>
+      <Hello name={nimi} age={ika}/>
     </div>
   )
 }
