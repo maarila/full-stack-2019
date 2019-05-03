996de93c87f56c28369f0e266164daa93b5d180e
Mika Äärilä <maarila@gmail.com>
2019-05-03 20:06:41 +0300

    Return 3c note examples

--- osa3-note-backend/index.js
+++ osa3-note-backend/index.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const Note = require('./models/note');
 
 app.use(express.static('build'));
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
@@ -24,15 +24,37 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note.toJSON());
-  });
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note.toJSON());
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter(note => note.id !== id);
-  res.status(204).end();
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => next(error));
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body;
+  const note = {
+    content: body.content,
+    important: body.important
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote.toJSON());
+    })
+    .catch(error => next(error));
 });
 
 app.post('/api/notes', (req, res) => {
@@ -64,6 +86,17 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
