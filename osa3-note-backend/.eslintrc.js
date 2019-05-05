f3956db5c3b98780395c1b24677d41534f933c6c
Mika Äärilä <maarila@gmail.com>
2019-05-05 18:12:20 +0300

    Refactor note app's structure

--- osa3-note-backend/.eslintrc.js
+++ osa3-note-backend/.eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'never'],
+    semi: ['error', 'always'],
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
--- osa3-note-backend/models/note.js
+++ osa3-note-backend/models/note.js
@@ -1,30 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.set('useFindAndModify', false);
-
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-
-mongoose
-  .connect(url, { useNewUrlParser: true })
-  .then(result => {
-    console.log('connected to MongoDB');
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+// mongoose.set('useFindAndModify', false);
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
-  date: {
-    type: Date,
-    required: true
-  },
+  date: Date,
   important: Boolean
 });
 
